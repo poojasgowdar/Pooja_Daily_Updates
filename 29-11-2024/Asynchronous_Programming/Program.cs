31a18b1c21c3d116ce7f32ad1bd42d0108229e43
pooja_s <poojagowdar19@gmail.com>
2024-11-29 18:08:38 +0530

    Friday Commit

---
+++ 29-11-2024/Asynchronous_Programming/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Statred");
+            Action actionDelegate = new Action(PrintCounter);
+            Task task1 = new Task(actionDelegate);
+            //You can directly pass the PrintCounter method as its signature is same as Action delegate
+            //Task task1 = new Task(PrintCounter);
+            task1.Start();
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+        }
+        static void PrintCounter()
+        {
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+            for (int count = 1; count <= 5; count++)
+            {
+                Console.WriteLine($"count value: {count}");
+            }
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+
+    }
+   
+}
---
+++ 29-11-2024/MulticastDelegateDemo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MulticastDelegateDemo2
+{
+    public delegate void SampleDelegate(out int Integer);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleDelegate del = new SampleDelegate(MethodOne);
+            del += MethodTwo;
+            // The ValueFromOutPutParameter will be 2, initialized by MethodTwo(),
+            // as it is the last method in the invocation list.
+            int ValueFromOutPutParameter = -1;
+            del(out ValueFromOutPutParameter);
+            Console.WriteLine($"Returned Value = {ValueFromOutPutParameter}");
+            Console.ReadKey();
+        }
+        // This method sets ouput parameter Number to 1
+        public static void MethodOne(out int Number)
+        {
+            Console.WriteLine("MethodOne is Executed");
+            Number = 1;
+        }
+        // This method sets ouput parameter Number to 2
+        public static void MethodTwo(out int Number)
+        {
+            Console.WriteLine("MethodTwo is Executed");
+            Number = 2;
+        }
+    }
+}
